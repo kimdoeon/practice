a0c280c7de313fdf53c082915f240af74b3ab798
kimdoeon <ilsin205@naver.com>
2022-03-12 16:29:32 +0900

    youtube_example

---
+++ vanilawebProjects/Youtube example/main.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".toggleBtn");
+const text = document.querySelector(".visible");
+
+toggle.addEventListener("click", ()=>{
+    text.classList.toggle("visible")
+    toggle.classList.toggle("clicked")
+}); 
