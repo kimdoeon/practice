fc01b223947175f85b38377d83c1a4d84a0dd385
kimdoeon <ilsin205@naver.com>
2022-03-12 16:30:11 +0900

    sample1

--- Responsive Web practice/sample1/README.md
+++
@@ -1,6 +0,0 @@
-# Responsive Website example
-* **HTML & CSS**
-* **Medium / Wide screen**
-
-#### reference
-http://bdmp-006.cafe24.com/bizdemo107829/
--- Responsive Web practice/sample1/main.js
+++
@@ -1,5 +0,0 @@
-// const toggle = document.querySelector(".main-menu");
-
-// toggle.addEventListener("click", ()=>{
-//     toggle.classList.toggle("visible")
-// });
