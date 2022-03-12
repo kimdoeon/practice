deb8af2ee0e0374997d2e7e062931d5a30ecc46d
kimdoeon <ilsin205@naver.com>
2022-03-12 16:30:22 +0900

    nav_bar

--- Responsive Web practice/nav bar/README.md
+++
@@ -1,3 +0,0 @@
-# Nav bar
-#### reference
-https://www.youtube.com/watch?v=X91jsJyZofw
--- Responsive Web practice/nav bar/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector(".nav__bar");
-const menu = document.querySelector(".nav__menu");
-const icons = document.querySelector(".nav__icons");
-
-toggleBtn.addEventListener('click',()=>{
-    menu.classList.toggle("active");
-    icons.classList.toggle("active");
-});
